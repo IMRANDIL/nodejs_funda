1faac057d2c85405eb49a651512e652f8ef57e3d
IMRANDIL <aliimranadil2@gmail.com>
2021-12-06 09:21:59 +0530

    yh

--- controlreadstream.js
+++ controlreadstream.js
@@ -14,4 +14,9 @@ const stream = createReadStream('./content/bigfile.txt', { highWaterMark: 90000,
 
 stream.on('data', (result) => {
     console.log(result);
+});
+
+
+stream.on('error', (err) => {
+    console.log(err);
 })
